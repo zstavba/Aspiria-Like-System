5d670effc8c0a8b1ee29f99ebcead6200e4981f6
Žan Stavbar <46623220+zstavba@users.noreply.github.com>
2021-03-02 11:28:40 +0100

    Update register.blade.php

--- resources/views/user/register.blade.php
+++ resources/views/user/register.blade.php
@@ -1,10 +1,18 @@
-<h1 class="mx-auto text-white text-3xl text-center mt-20 pb-4 border-opacity-25 border-b-2 border-white w-1/2">
+<h1 class="mx-auto text-white text-3xl text-center mt-5 -m-2 pb-4 border-opacity-25 border-b-2 border-white w-1/2">
 	Nov uporabnik
 </h1>
 <div class="row mx-auto  w-5/6  m-10 bg-white rounded p-5" style="height: auto; min-height: 50vh;">
 	<div class="mb-5">
 	  <label for="email" class="block text-sm font-medium text-gray-500 text-xl">
-	  	Uprabniško ime / e-naslov
+	  	Ime in Priimek 
+	  </label>
+	  <div class="mt-1">
+	    <input type="text" name="email" id="email" class="p-2 w-full border-indigo-500 border-b-2 focus:border-indigo-700 focus:border-b-2 focus:outline-none" placeholder="Uproabniško ime / e-naslov">
+	  </div>
+	</div>
+	<div class="mb-5">
+	  <label for="email" class="block text-sm font-medium text-gray-500 text-xl">
+	  	E-naslov
 	  </label>
 	  <div class="mt-1">
 	    <input type="text" name="email" id="email" class="p-2 w-full border-indigo-500 border-b-2 focus:border-indigo-700 focus:border-b-2 focus:outline-none" placeholder="Uproabniško ime / e-naslov">
